cbd430dd6a223e633a20b465f18fb338868c58e3
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-02-10 14:43:50 +0200

    added footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="wrapper__footer footer">
+      <div className="footer__wrapper container">
+        <p>Copyright © 2022 Anastasiia Erokhina. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
