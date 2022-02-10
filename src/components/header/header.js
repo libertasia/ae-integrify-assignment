0dc898854aa7c27d7a426c909a66e4fca5e5a39b
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-02-10 14:42:18 +0200

    added header component

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="wrapper__header header">
+      <div className="header__wrapper container">
+        <p>Integrify Full-Stack course React Pre-Assignment</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
