95933c221c5523c87d2ca5daab7ea5f517b442b3
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-02-10 16:20:54 +0200

    modified main-screen, user-screen, const

--- src/components/main-screen/main-screen.js
+++ src/components/main-screen/main-screen.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import UsersList from '../users-list/users-list';
 
 const MainScreen = () => {
   return (
     <React.Fragment>
-
+      <Header />
+      <main>
+        <h1 className="visually-hidden">Users page</h1>
+        <section>
+          <h2 className="visually-hidden">Users list</h2>
+          <UsersList />
+        </section>
+      </main>
+      <Footer />
     </React.Fragment>
   );
 };
--- src/components/user-screen/user-screen.js
+++ src/components/user-screen/user-screen.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
 
 const UserScreen = () => {
   return (
     <React.Fragment>
-
+      <Header />
+      <main>
+        <h1 className="visually-hidden">User info page</h1>
+        <section>
+          <h2 className="visually-hidden">Detail user's info</h2>
+        </section>
+      </main>
+      <Footer />
     </React.Fragment>
   );
 };
