7ff99de136bfb425841d49c097bdfeadcdfe4c67
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-02-11 17:32:09 +0200

    styling user-info page WIP

--- src/components/user-screen/user-screen.js
+++ src/components/user-screen/user-screen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import { Avatar } from '@mui/material';
 import { createAPI } from '../../services/api';
 
 const api = createAPI();
@@ -36,23 +37,32 @@ const UserScreen = () => {
         <main className="main container">
           <h1 className="main__title visually-hidden">User info page</h1>
           <section className="user-info">
-            <h2 className="user-info__title visually-hidden">Detail user's info</h2>
+            <h2 className="user-info__title">Detailed user info</h2>
             <article className="user-info__card">
-              <h3 className="user-info__card-title">User info</h3>
-              <p>name: {user.name}</p>
-              <p>username: &#64;{user.username}</p>
-              <p>company: {user.company.name}</p>
-              <address>
-                <p>email: <a href={`mailto:${user.email}`}>{user.email}</a></p>
-                <p>phone: <a href={`tel:${user.phone}`}>{user.phone}</a></p>
-                <p>website: <Link to={user.website}>{user.website}</Link></p>
-                <p>address:<br />
-                  <span>street: {user.address.street}</span>
-                  <span>suite: {user.address.suite}</span>
-                  <span>city: {user.address.city}</span>
-                  <span>zipcode: {user.address.zipcode}</span>
-                </p>
-              </address>
+              <h3 className="user-info__card-title visually-hidden">User info card</h3>
+              <div className="user-info__avatar">
+                <Avatar className="user-info__img"
+                    sx={{width: 80, height: 80}}
+                >
+                  {user.name.charAt(0)}
+                </Avatar>
+              </div>
+              <div className="user-info__details">
+                <p>name: {user.name}</p>
+                <p>username: &#64;{user.username}</p>
+                <p>company: {user.company.name}</p>
+                <address>
+                  <p>email: <a href={`mailto:${user.email}`}>{user.email}</a></p>
+                  <p>phone: <a href={`tel:${user.phone}`}>{user.phone}</a></p>
+                  <p>website: <Link to={user.website}>{user.website}</Link></p>
+                  <p>address:<br />
+                    <span>street: {user.address.street}</span>
+                    <span>suite: {user.address.suite}</span>
+                    <span>city: {user.address.city}</span>
+                    <span>zipcode: {user.address.zipcode}</span>
+                  </p>
+                </address>
+              </div>
             </article>
           </section>
         </main>
